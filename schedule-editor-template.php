f916fae8623eb90a03a0850e38a42e1a52c6ff95
TereseBo <terese.boderus@gmail.com>
2023-09-10 17:06:19 +0200

    Added headings for weekdays.

--- schedule-editor-template.php
+++ schedule-editor-template.php
@@ -1,11 +1,17 @@
-
 <div class="schedule">
     <?php
     $weekarray = array('monday', 'tuesday', 'wednesday', 'thursday', 'friday');
     foreach ($weekarray as $value) {
         ?>
-        <div class=WD-con>
+        <div class=weekday-container>
+            <div class=weekday-heading>
+                <?php
+                echo '<h3>' . ucfirst($value) . '</h3>  ';
+                ?>
+            </div>
+
             <?php
+//Query for each weekday ($value) and fetch all schedule_contents for this day. Order asc by starttime.
             $the_query = new WP_Query(
                 array(
                     'post_type' => 'schedule_content',
@@ -27,25 +33,24 @@
                 ?>
                 <div class="schedule-entry">
                     <?php the_title();
-                    /*Add boxes, use if to change order*/
+                    //Get all info to be displayed for each post
                     $lesson_name = get_the_terms($the_query->ID, 'schedule_subject')[0]->name;
-                    $weekday_name = get_the_terms($the_query->ID, 'schedule_weekday')[0]->name;
                     $start_time = get_the_terms($the_query->ID, 'schedule_starttime')[0]->name;
                     $end_time = get_the_terms($the_query->ID, 'schedule_endtime')[0]->name;
 
 
-                    // Display the taxonomy name if previous and current post term name don't match
+                    //Use separate template for lesson-contents due to readability
                     include(plugin_dir_path(__FILE__) . 'schedule-content-template.php');
                     ?>
                 </div>
                 <?php
             endwhile;
             ?>
-            </div>
-            <?php
+        </div>
+        <?php
 
 
     }
     ?>
 
-    </div>
+</div>
